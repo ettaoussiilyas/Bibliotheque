ed0f74723451dd6a99a6a5931bce2d2159b95038
ettaoussi <ettaoussiilyas0.1@gmail.com>
2024-12-24 16:38:41 +0100

    BIB-6 creation of class database has connection on contructor and disconect method and get connection

---
+++ config/db.php
@@ -0,0 +1,38 @@
+<?php
+
+class DataBase{
+    private $host = "localhost";
+    private $db_name = "bibliotheque";
+    private $username = "root";
+    private $password = "";
+    private $conn;
+    private $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false
+    );
+
+    public function __construct(){
+        $this->conn = null;
+        // self::$conn = null;
+        try{
+            $this->conn = new PDO("mysql:host=".$this->host.";dbname=".$this->db_name, $this->username, $this->password, $this->options);
+            echo "Connected successfully";
+        }catch(PDOException $exception){
+            echo "Connection error: ".$exception->getMessage();
+        }
+    }
+
+    public function disconnect(){
+        $this->conn = null;
+    }
+
+    public function getConnection(){
+        return $this->conn;
+    }
+    
+
+}
+
+
+?>
